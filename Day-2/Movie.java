3483cfe8ccbdb5bb05273ad4c742ef360da1bb56
Neutrino Highland <neutrino3928@outlook.com>
2025-06-16 18:30:00 -0500

    new file

---
+++ Day-2/Movie.java
@@ -0,0 +1,12 @@
+public class Movie {
+    // Movie attributes (variables)
+
+    // Movie constructor
+
+    // Movie getter and setters
+
+    // Method calls
+    public static void main(String[] args) {
+
+    }
+}
