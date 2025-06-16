1640a00b1cb0611078de0b7f7d30655e3c0c394b
Neutrino Highland <neutrino3928@outlook.com>
2025-06-16 18:33:17 -0500

    done

---
+++ Day-2/Movie.java
@@ -0,0 +1,12 @@
+public class Movie {
+    // Movie attributes (variables)
+
+    // Movie constructor
+
+    // Movie getter and setters
+
+    // Method calls (inside main method)
+    public static void main(String[] args) {
+
+    }
+}
