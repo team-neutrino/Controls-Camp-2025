c71bd1f8d22a8cf4723eb7051ee740f483e5ed5c
Neutrino Angus <neutrino3928@outlook.com>
2025-06-27 19:06:27 -0500

    correct length

--- Electrical-Camp/src/main/java/frc/robot/Constants.java
+++ Electrical-Camp/src/main/java/frc/robot/Constants.java
@@ -22,7 +22,7 @@ public final class Constants {
   }
 
   public final class LEDConstants {
-    public static final int LEDBufferLen = 140;
+    public static final int LEDBufferLen = 10;
     public static final int LED = 0;
   }
 }
