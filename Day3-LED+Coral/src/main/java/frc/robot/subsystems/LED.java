34962a6be49f170a36c8ef5db19ee46dd80f977b
Neutrino - Florida Cracker <neutrino3928@outlook.com>
2025-07-07 17:09:33 -0500

    Setup LED problem

--- Day3-LED+Coral/src/main/java/frc/robot/subsystems/LED.java
+++ Day3-LED+Coral/src/main/java/frc/robot/subsystems/LED.java
@@ -9,13 +9,22 @@ import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj2.command.*;
 
 public class LED extends SubsystemBase {
+  private static final int BEAMBREAK_PORT = 1;
+  private static final int ROBORIO_LED_PWM_PORT = 7;
+  private static final int LED_STRIP_PIXELS = 8;
+  private AddressableLED m_addressableLED = new AddressableLED(ROBORIO_LED_PWM_PORT);
+  private AddressableLEDBuffer m_LEDBuffer = new AddressableLEDBuffer(LED_STRIP_PIXELS);
+  private DigitalInput m_beambreak = new DigitalInput(BEAMBREAK_PORT);
 
   public LED() {
-
+    m_addressableLED.setLength(m_LEDBuffer.getLength());
+    m_addressableLED.setData(m_LEDBuffer);
+    m_addressableLED.start();
   }
 
   @Override
   public void periodic() {
+    m_addressableLED.setData(m_LEDBuffer);
   }
 
   public Command doNothing() {
@@ -27,6 +36,8 @@ public class LED extends SubsystemBase {
   public Command colorCommand(int r, int g, int b) {
     return run(
         () -> {
-        });
+          for(int i = 0; i < m_LEDBuffer.getLength(); ++i) {
+            m_LEDBuffer.setRGB(i, r, g, b);
+        }});
   }
 }
