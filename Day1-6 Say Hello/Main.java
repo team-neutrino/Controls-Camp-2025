67b3b46b4e28ae3f73512fec5518c78c76a6b0c9
Neutrino Palkia <neutrino3928@outlook.com>
2025-07-14 14:51:29 -0500

    Day 1 folder name changes

---
+++ Day1-6 Say Hello/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+    }
+
+    //Create your method here
+    public static () {
+
+    }
+}
---
+++ Day1-7 Do Math/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+    }
+
+    //Create your Method here
+    public static () {
+
+    }
+}
