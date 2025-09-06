f28b13ddb6afe8ef0e5e7abc9e096be3e34976d4
Neutrino Palkia <neutrino3928@outlook.com>
2025-09-06 06:14:09 -0500

    Progress

--- Day-2/Main.java
+++ Day-2/Main.java
@@ -1,7 +1,14 @@
 public class Main {
-
-    // Put start place of the script inside the main method.
-    public static void main(String[] args) {
-
+   
+    // Put start place of the script inside the main method. 
+    
+    public static void main(String[] args) 
+    {private static int = 5;
+    private static double = 5.5;
+    private static String = "hello"; 
+    private static boolean = true;
+   
+    public static void printVariables(int) 
+        
     }
 }
